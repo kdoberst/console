4ded357ab966b971e4b1304fb4f5e4adf0965b36
Samuel Padgett <spadgett@redhat.com>
2021-04-14 13:05:55 -0400

    [WIP] pass cluster list through JS globals

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -97,6 +97,7 @@ type jsGlobals struct {
 	ConsolePlugins            []string `json:"consolePlugins"`
 	QuickStarts               string   `json:"quickStarts"`
 	ProjectAccessClusterRoles string   `json:"projectAccessClusterRoles"`
+	Clusters                 []string  `json:"clusters"`
 }
 
 type Server struct {
@@ -181,8 +182,8 @@ func (s *Server) HTTPHandler() http.Handler {
 		}
 	}
 
-	localK8sProxyConfig := s.K8sProxyConfigs["local-cluster"];
-	k8sProxies := make(map[string]*proxy.Proxy);
+	localK8sProxyConfig := s.K8sProxyConfigs["local-cluster"]
+	k8sProxies := make(map[string]*proxy.Proxy)
 	for cluster, proxyConfig := range s.K8sProxyConfigs {
 		k8sProxies[cluster] = proxy.NewProxy(proxyConfig)
 	}
@@ -270,7 +271,6 @@ func (s *Server) HTTPHandler() http.Handler {
 	handleFunc("/health", health.Checker{
 		Checks: []health.Checkable{},
 	}.ServeHTTP)
-
 
 	handle(k8sProxyEndpoint, http.StripPrefix(
 		proxy.SingleJoiningSlash(s.BaseURL.Path, k8sProxyEndpoint),
@@ -516,6 +516,16 @@ func (s *Server) indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	plugins := make([]string, 0, len(s.EnabledConsolePlugins))
+	for plugin := range s.EnabledConsolePlugins {
+		plugins = append(plugins, plugin)
+	}
+
+	clusters := make([]string, 0, len(s.K8sProxyConfigs))
+	for cluster := range s.K8sProxyConfigs {
+		clusters = append(clusters, cluster)
+	}
+
 	jsg := &jsGlobals{
 		ConsoleVersion:            version.Version,
 		AuthDisabled:              s.authDisabled(),
@@ -542,10 +552,11 @@ func (s *Server) indexHandler(w http.ResponseWriter, r *http.Request) {
 		GraphQLBaseURL:            proxy.SingleJoiningSlash(s.BaseURL.Path, graphQLEndpoint),
 		DevCatalogCategories:      s.DevCatalogCategories,
 		UserSettingsLocation:      s.UserSettingsLocation,
-		ConsolePlugins:            getMapKeys(s.EnabledConsolePlugins),
+		ConsolePlugins:            plugins,
 		QuickStarts:               s.QuickStarts,
 		AddPage:                   s.AddPage,
 		ProjectAccessClusterRoles: s.ProjectAccessClusterRoles,
+		Clusters:                  clusters,
 	}
 
 	if !s.authDisabled() {
@@ -639,12 +650,4 @@ func tokenToObjectName(token string) string {
 	name := strings.TrimPrefix(token, sha256Prefix)
 	h := sha256.Sum256([]byte(name))
 	return sha256Prefix + base64.RawURLEncoding.EncodeToString(h[0:])
-}
-
-func getMapKeys(m map[string]string) []string {
-	keys := []string{}
-	for key, _ := range m {
-		keys = append(keys, key)
-	}
-	return keys
 }
